ec020c0533f987e521a4329e67df96c346f04c24
Pedro K. Humire <pedrohumirer@gmail.com>
2023-01-17 14:48:52 +0100

    Add files via upload
    coordconverter.py converts from degree to hour:minutes:seconds and vice-versa

---
+++ kinematics/coordconverter.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-   #con esto puedo poner tilde.
+
+def deg2HMS(ra='', dec='', round=False):
+  RA, DEC, rs, ds = '', '', '', ''
+  if dec:
+    if str(dec)[0] == '-':
+      ds, dec = '-', abs(dec)
+    deg = int(dec)
+    decM = abs(int((dec-deg)*60))
+    if round:
+      decS = int((abs((dec-deg)*60)-decM)*60)
+    else:
+      decS = (abs((dec-deg)*60)-decM)*60
+    DEC = '{0}{1} {2} {3}'.format(ds, deg, decM, decS)
+  
+  if ra:
+    if str(ra)[0] == '-':
+      rs, ra = '-', abs(ra)
+    raH = int(ra/15)
+    raM = int(((ra/15)-raH)*60)
+    if round:
+      raS = int(((((ra/15)-raH)*60)-raM)*60)
+    else:
+      raS = ((((ra/15)-raH)*60)-raM)*60
+    RA = '{0}{1} {2} {3}'.format(rs, raH, raM, raS)
+  
+  if ra and dec:
+    return (RA, DEC)
+  else:
+    return RA or DEC
+
+
+def deg2HMSra(ra='', round=False):
+  RA, rs = '', ''
+  if ra:
+    if str(ra)[0] == '-':
+      rs, ra = '-', abs(ra)
+    raH = int(ra/15)
+    raM = int(((ra/15)-raH)*60)
+    raS = ((((ra/15)-raH)*60)-raM)*60
+    RA = '{0}{1}h{2}m{3:.0f}s'.format(rs, raH, raM, raS)
+  
+    return RA 
+
+def deg2HMSdec(dec='', round=False):
+  DEC, ds = '', ''
+  if dec:
+    if str(dec)[0] == '-':
+      ds, dec = '-', abs(dec)
+    deg = int(dec)
+    decM = abs(int((dec-deg)*60))
+    decS = (abs((dec-deg)*60.0)-decM)*60.0
+    DEC = '{0}{1}$^o${2}\'{3:.2f}\"'.format(ds, deg, decM, decS)
+
+    return DEC
+
+def HMS2deg(ra='', dec=''):
+  RA, DEC, rs, ds = '', '', 1, 1
+  if dec:
+    D, M, S = [float(i) for i in dec.split()]
+    if str(D)[0] == '-':
+      ds, D = -1, abs(D)
+    deg = D + (M/60) + (S/3600)
+    DEC = '{0}'.format(deg*ds)
+  
+  if ra:
+    H, M, S = [float(i) for i in ra.split()]
+    if str(H)[0] == '-':
+      rs, H = -1, abs(H)
+    deg = (H*15) + (M/4) + (S/240)
+    RA = '{0}'.format(deg*rs)
+  
+  if ra and dec:
+    return (RA, DEC)
+  else:
+    return RA or DEC
